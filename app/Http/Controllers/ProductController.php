55b9708f94930328cdbb3c7a4b465850b4f93e04
richard6410 <tetsu.t6410@gmail.com>
2023-10-20 23:27:51 +0900

    Initial Commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,13 +6,11 @@ use App\Models\Product;
 use App\Models\Maker;
 use Illuminate\Http\Request;
 
+
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // index メソッド
+
     public function index()
     {
         $products = product::select([
@@ -36,124 +34,53 @@ class ProductController extends Controller
             ->with('i',(request()->input('page',1) -1)*5);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         $makers = Maker::all();
-        return view('create')
-            ->with('page_id',request()->page_id)
-            ->with('makers',$makers);
+        return view('create')->with('page_id', request()->page_id)->with('makers', $makers);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $request->validate([
-            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
-            'syouhinmei' => 'required|max:20',
-            'maker' => 'required|integer',
-            'kakaku' => 'required|integer',
-            'zaikosuu' => 'required|integer',
-            'comment' => 'required|max:100',
+            // バリデーションルール
         ]);
-        
-        $product = new Product;
-        
 
-        if ($request->hasFile('image')) {
-            $original = $request->file('image')->getClientOriginalName();
-            $name = date('Ymd_His') . '_' . $original;
-            $file = $request->file('image')->storeAs('public/images', $name); // 画像を指定ディレクトリに保存
-            $product->image = $name; // ファイル名をデータベースに格納
-        }
- 
-        $product->syouhinmei = $request->input('syouhinmei');
-        $product->maker = $request->input('maker');
-        $product->kakaku = $request->input('kakaku');
-        $product->zaikosuu = $request->input('zaikosuu');
-        $product->comment = $request->input('comment');
-        $product->save();
-       
+        $data = $request->all();
+        Product::createProduct($data);
 
+        return redirect()->route('product.create')->with('success', '登録しました');
+    }
 
-        return redirect()->route('product.create')->with('success','登録しました');
-    } 
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
     public function show(Product $product)
     {
         $makers = Maker::all();
-        return view('show',compact('product','makers'))
-        ->with('page_id',request()->page_id)
-        ->with('makers',$makers);
-
+        return view('show', compact('product', 'makers'))->with('page_id', request()->page_id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Product $product)
     {
-        $makers = maker::all();
-        return view('edit',compact('product','makers'))
-        ->with('page_id',request()->page_id);
+        $makers = Maker::all();
+        return view('edit', compact('product', 'makers'))->with('page_id', request()->page_id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Product $product)
     {
         $request->validate([
-            'syouhinmei' => 'required|max:20',
-            'maker' => 'required|integer',
-            'kakaku' => 'required|integer',
-            'zaikosuu' => 'required|integer',
-            'comment' => 'required|max:100',
+            // バリデーションルール
         ]);
-        
-        $product->syouhinmei = $request->input('syouhinmei');
-        $product->maker = $request->input('maker');
-        $product->kakaku = $request->input('kakaku');
-        $product->zaikosuu = $request->input('zaikosuu');
-        $product->comment = $request->input('commment');
-        $product->save();
-       
-        return redirect()->route('products.index')->with('success','変更しました'); 
+
+        $data = $request->all();
+        Product::updateProduct($product, $data);
+
+        return redirect()->route('products.index')->with('success', '変更しました');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Product $product)
     {
         $product->delete();
-        return redirect()->route('products.index')
-            ->with('success','商品'.$product->syouhinmei.'を削除しました');
+        return redirect()->route('products.index')->with('success', '商品'.$product->syouhinmei.'を削除しました');
     }
 
     public function search(Request $request)
@@ -161,28 +88,10 @@ class ProductController extends Controller
         $syouhinmei = $request->input('syouhinmei');
         $maker = $request->input('maker');
 
-        $query = product::select([
-            'i.id',
-            'i.syouhinmei',
-            'i.kakaku',
-            'i.zaikosuu',
-            'm.str as maker',
-        ])
-        ->from('products as i')
-        ->join('makers as m','i.maker','m.id');
-    
-        if ($syouhinmei) {
-            $query->where('syouhinmei', 'like', '%' . $syouhinmei . '%');
-        }
-    
-        if ($maker) {
-            $query->where('m.str',$maker);
-        }
-        $products = $query->paginate(10);
+        $products = Product::searchProducts($syouhinmei, $maker);
         $page_id = $request->input('page_id');
 
-
-        return view('index',compact('products','page_id'));
-    } 
-
+        return view('index')->with(['products' => $products, 'page_id' => $page_id]);
+    }
 }
+
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,102 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public static function getAllProducts()
+    {
+        return Product::select([
+            'b.id',
+            'b.image',
+            'b.syouhinmei',
+            'b.kakaku',
+            'b.zaikosuu',
+            'b.comment',
+            'r.str as maker',
+        ])
+        ->from('products as b')
+        ->join('makers as r', function ($join) {
+            $join->on('b.maker', 'r.id');
+        })
+        ->orderBy('b.id', 'DESC')
+        ->paginate(5);
+    }
+
+    public static function createProduct($data)
+    {
+        // 新しい商品を作成してデータベースに保存するロジックを追加
+        $product = new Product;
+
+    if (isset($data['image']) && $data['image']->isValid()) {
+        $original = $data['image']->getClientOriginalName();
+        $name = date('Ymd_His') . '_' . $original;
+        $path = $data['image']->storeAs('public/images', $name);
+
+        // 画像ファイルが正常にアップロードされた場合、ファイル名をデータベースに格納
+        $product->image = $name;
+    }
+
+    $product->syouhinmei = $data['syouhinmei'];
+    $product->maker = $data['maker'];
+    $product->kakaku = $data['kakaku'];
+    $product->zaikosuu = $data['zaikosuu'];
+    $product->comment = $data['comment'];
+
+    $product->save();
+
+    return $product;
+    }
+
+    public static function updateProduct($product, $data)
+    {
+        // 商品を更新するロジックを追加
+        if (isset($data['image']) && $data['image']->isValid()) {
+            // 画像ファイルがアップロードされた場合、新しい画像を保存
+            $original = $data['image']->getClientOriginalName();
+            $name = date('Ymd_His') . '_' . $original;
+            $path = $data['image']->storeAs('public/images', $name);
+    
+            // 古い画像ファイルを削除
+            if ($product->image) {
+                Storage::delete('public/images/' . $product->image);
+            }
+    
+            // 新しいファイル名をデータベースに格納
+            $product->image = $name;
+        }
+    
+        $product->syouhinmei = $data['syouhinmei'];
+        $product->maker = $data['maker'];
+        $product->kakaku = $data['kakaku'];
+        $product->zaikosuu = $data['zaikosuu'];
+        $product->comment = $data['comment'];
+    
+        $product->save();
+    
+        return $product;
+    }
+
+
+    public static function searchProducts($syouhinmei, $maker)
+    {
+        // 商品を検索するロジックを追加
+        $query = Product::select([
+            'i.id',
+            'i.syouhinmei',
+            'i.kakaku',
+            'i.zaikosuu',
+            'm.str as maker',
+        ])
+        ->from('products as i')
+        ->join('makers as m', 'i.maker', '=', 'm.id');
+    
+        if ($syouhinmei) {
+            $query->where('i.syouhinmei', 'like', '%' . $syouhinmei . '%');
+        }
+    
+        if ($maker) {
+            $query->where('m.str', $maker);
+        }
+    
+        return $query->paginate(10);
+    }
 }
