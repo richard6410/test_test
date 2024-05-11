ee7f247e1d6df8df3d133e1d32c9cad2e783ebc3
richard6410 <tetsu.t6410@gmail.com>
2024-05-12 03:57:40 +0900

    コミットメッセージ

--- resources/views/search.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{{ route('product.search') }}" method="GET">
-    <div class="form-group">
-        <input type="text" name="keyword" class="form-control" placeholder="商品名を検索">
-    </div>
-    <button type="submit" class="btn btn-primary">検索</button>
-</form>
